6a35ce5591203724910a3ad5df9dd6b58340066c
Florian Wilhelm <Florian.Wilhelm@inovex.de>
2019-09-21 19:03:08 +0200

    Update to Pyscaffold 3.2.2

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
     Setup file for configupdater.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 3.1.
+    This file was generated with PyScaffold 3.2.2.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
