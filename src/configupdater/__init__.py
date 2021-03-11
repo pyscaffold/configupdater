09f51c0261acf30f8daa17274c8a439995b95a4f
Anderson Bravalheri <andersonbravalheri@gmail.com>
2021-03-11 18:09:59 +0000

    Update from PyScaffold v4.0.1rc2
    The changes in this commit were generated by doing `putup -U -f .` and
    then using `git difftool` to merge the differences back.
    
    There are some small improvements, such as fixing setuptools warnings
    due to kebab-case on setup.cfg

--- src/configupdater/__init__.py
+++ src/configupdater/__init__.py
@@ -2,15 +2,15 @@ import sys
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 else:
-    from importlib_metadata import PackageNotFoundError, version
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "ConfigUpdater"
     __version__ = version(dist_name)
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
