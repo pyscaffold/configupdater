c2819815346c12d405dff7a5f6f78b22a5c2f929
Anderson Bravalheri <andersonbravalheri@gmail.com>
2024-09-24 11:37:13 +0100

    Update using PyScaffold 4.6

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 """
-    Setup file for ConfigUpdater.
-    Use setup.cfg to configure your project.
+Setup file for ConfigUpdater.
+Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.0.1rc2.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
+This file was generated with PyScaffold 4.6.
+PyScaffold helps you to put up the scaffold of your new Python project.
+Learn more under: https://pyscaffold.org/
 """
 
 from setuptools import setup
